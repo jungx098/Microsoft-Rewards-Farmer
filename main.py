67434ae0562a7a57d3aaf81556d6bc18f74e3277
jungx098 <jungx098@gmail.com>
2024-06-26 05:15:22 -0500

    Disable ReadToEarn

--- main.py
+++ main.py
@@ -212,7 +212,10 @@ def executeBot(currentAccount, args: argparse.Namespace):
         logging.info(
             f"[POINTS] You have {utils.formatNumber(accountPointsCounter)} points on your account"
         )
-        ReadToEarn(desktopBrowser).completeReadToEarn(startingPoints)
+
+        # ReadToEarn is disabled for further test as it does not use webdriver.
+        # ReadToEarn(desktopBrowser).completeReadToEarn(startingPoints)
+
         DailySet(desktopBrowser).completeDailySet()
         PunchCards(desktopBrowser).completePunchCards()
         MorePromotions(desktopBrowser).completeMorePromotions()
